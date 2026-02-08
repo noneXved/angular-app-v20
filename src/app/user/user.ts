f99e428f1345fe3075d77e93d9fa2499fa76669c
Damian Bajno <bajno.damian96@gmail.com>
2026-02-08 20:19:50 +0100

    Initial commit

---
+++ src/app/user/user.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+  selectedUser = DUMMY_USERS[randomIndex];
+}
